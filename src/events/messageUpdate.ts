6c3ad0257064d63e74a185df8f6939e17ed67146
Torwent <Torwent@users.noreply.github.com>
2026-06-13 10:33:26 +0100

    fix: make it so the new feature ignores scripter, mods and admins messages

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,7 +1,8 @@
 import { Events } from "discord.js"
 import { ClientEvent } from "$lib/client"
+import { getRole } from "$lib/utils"
 
-const hour = 30 * 60 * 1000
+const maxTime = 30 * 60 * 1000
 
 export default new ClientEvent(Events.MessageUpdate, async (client, oldMsg, newMsg) => {
 	const { guild } = newMsg
@@ -10,16 +11,24 @@ export default new ClientEvent(Events.MessageUpdate, async (client, oldMsg, newM
 
 	const ageMs = (newMsg.editedTimestamp ?? 0) - oldMsg.createdTimestamp
 
-	if (ageMs > hour) {
-		const reply = await newMsg
-			.reply(
-				"<@" +
-					newMsg.author.id +
-					"> your message has been deleted.\n\nFor server safety reasons, you can't edit messages that are older than 30 minutes on this server."
-			)
-			.catch((e) => console.error(e))
-		await newMsg.delete().catch((e) => console.error(e))
+	if (ageMs <= maxTime) return
 
-		if (reply) setTimeout(async () => await reply.delete(), 15000)
-	}
+	const { member } = newMsg
+	if (!member) return
+	console.log(member.guild.id)
+	const role = getRole(member, ["administrator", "moderator", "scripter"])
+	if (role) return
+
+	const reply = await newMsg
+		.reply(
+			"<@" +
+				newMsg.author.id +
+				"> your message has been deleted.\n\nFor server safety reasons, you can't edit messages that are older than " +
+				(maxTime / 60 / 1000).toString() +
+				" minutes on this server."
+		)
+		.catch((e) => console.error(e))
+	await newMsg.delete().catch((e) => console.error(e))
+
+	if (reply) setTimeout(async () => await reply.delete(), 15000)
 })
